c194c176b273d32671f9c748231b2acbef1662d0
codetiv <100881647+codetiv@users.noreply.github.com>
2023-03-03 21:20:02 -0700

    add employee list for import exercise

---
+++ Exercise/importing_exercise/employee.py
@@ -0,0 +1,8 @@
+employee_name = "Timmy"
+age = "100"
+title = "Owner"
+
+def details():
+    print("Employee name is: ", employee_name)
+    print("Employee age is: ", age)
+    print("Employee title is: ", title)
